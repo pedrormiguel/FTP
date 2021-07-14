ddcde3b881441404bf5ee14eaf02243991c19115
Pedro Ruiz <pedrormiguel@outlook.es>
2021-07-14 18:17:12 -0400

    Added interfaces

--- src/Library/FTPLib/Interfaces/IFtp.cs
+++ src/Library/FTPLib/Interfaces/IFtp.cs
@@ -1,7 +1,14 @@
+using System.Threading.Tasks;
+using FTPLib.Class.Common;
+
 namespace FTPLib.Interfaces
 {
     public interface IFtp
     {
-        
+        Response Connect();
+        Task<Response> GetListItems();
+        Task<Response> GetListItemsFiles(string folderPath);
+        Task<Response> UploadFile(string localPath, string remotePath);
+        Task<Response> DownloadFile(string localPathToDownload, string remotePathFile);
     }
 }
