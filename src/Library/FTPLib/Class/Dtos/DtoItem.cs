79d4dc14daa32e2e5dfc04beae2e23097527d755
Pedro Ruiz <pedrormiguel@outlook.es>
2021-07-14 18:15:30 -0400

    Added Dto for the object Item

--- src/Library/FTPLib/Class/Dtos/DtoItem.cs
+++ src/Library/FTPLib/Class/Dtos/DtoItem.cs
@@ -2,6 +2,13 @@ namespace FTPLib.Class.Dtos
 {
     public class DtoItem
     {
-        
+        public string FullName { get; set; }
+        public string OwnerPermissions { get; set; }
+        public long Size { get; set; }
+
+        public DtoItem Map(string fullName, string ownerPermissions, long size)
+        {
+            return new DtoItem() {FullName = fullName, Size = size, OwnerPermissions = ownerPermissions};
+        }
     }
 }
