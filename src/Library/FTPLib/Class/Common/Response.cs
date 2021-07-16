14c0ff6f8e59a2a13565131c9e744af9cf35fdee
Pedro Ruiz <pedrormiguel@outlook.es>
2021-07-15 20:29:56 -0400

    Added Generic type for Response

--- src/Library/FTPLib/Class/Common/Response.cs
+++ src/Library/FTPLib/Class/Common/Response.cs
@@ -2,12 +2,11 @@ using System;
 
 namespace FTPLib.Class.Common
 {
-    public class Response
+    public class Response<T>
     {
         public bool Status { get; set; } = false;
-        public Object Data { get; set; } = null;
+        public T Data { get; set; } 
         public string Error { get; set;}
-
         public void ErrorMapException(Exception ex)
         {
             Error = $"Error's {ex.Data} \n {ex.Message} \n {ex.Source} {ex.TargetSite}";
