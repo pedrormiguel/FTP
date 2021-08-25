4f8c655b447bdb57d8a1ddf3af018921579e45ee
Pedro Ruiz <pedrormiguel@outlook.es>
2021-08-25 10:04:00 -0400

    Override method ToString to return the credential info

--- src/CORE/Domain/Entities/Credential.cs
+++ src/CORE/Domain/Entities/Credential.cs
@@ -8,5 +8,10 @@ namespace CORE.Domain.Entities
         public string UserName { get; set; }
         public string Password { get; set; }
         public int Port { get; set; } = 21;
+
+        public override string ToString()
+        {
+            return $"{Id};{HostName};{UserName};{Password};{Port};";
+        }
     }
 }
