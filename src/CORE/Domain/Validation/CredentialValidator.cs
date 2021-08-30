5e6964a51ce6bafcadae8ee844853f885471432d
Pedro Ruiz <pedrormiguel@outlook.es>
2021-08-30 09:41:10 -0400

    Added Custom Validation for Credential Model.

---
+++ src/CORE/Domain/Validation/CredentialValidator.cs
@@ -0,0 +1,23 @@
+﻿using CORE.Domain.Entities;
+using FluentValidation;
+
+namespace CORE.Domain.Validation
+{
+    public class CredentialValidator : AbstractValidator<Credential>
+    {
+        public CredentialValidator()
+        {
+            RuleFor(x => x.HostName).NotEmpty()
+                                    .NotNull()
+                                    .WithMessage("HostName cannot be Empty");
+
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName cannot be Empty");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be Empty");
+
+            RuleFor(x => x.Port).NotEmpty()
+                                .GreaterThan(0)
+                                .WithMessage("Port cannot be Empty");
+        }
+    }
+}
