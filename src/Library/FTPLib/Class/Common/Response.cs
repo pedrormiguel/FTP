b72e4d9e14ffaae183255c4f75d5fda9020607b2
Pedro Ruiz <pedrormiguel@outlook.es>
2021-09-02 16:24:20 -0400

    Modified Response to follow the BaseResponse.

--- src/Library/FTPLib/Class/Common/Response.cs
+++ src/Library/FTPLib/Class/Common/Response.cs
@@ -1,16 +1,17 @@
 using System;
+using CORE.Application.Responses;
 
 namespace FTPLib.Class.Common
 {
-    public class Response<T>
+    public class Response<T> : BaseResponse<T>
     {
-        public bool Status { get; set; }
         public T Data { get; set; }
-        public string Error { get; set; }
+        public string Error { get; private set; }
 
         public void ErrorMapException(Exception ex)
         {
             Error = $"Error's {ex.Data} \n {ex.Message} \n {ex.Source} {ex.TargetSite}";
+            Success = false;
         }
     }
 }
