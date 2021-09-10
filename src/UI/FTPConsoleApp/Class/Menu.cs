6a0e9f696bff56c3d99970d491624d3da58114af
Pedro Ruiz <pedrormiguel@outlook.es>
2021-09-09 21:07:45 -0400

    Removed unnecesary statement.

--- src/UI/FTPConsoleApp/Class/Menu.cs
+++ src/UI/FTPConsoleApp/Class/Menu.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
+ using System.Collections.Generic;
 using System.Threading.Tasks;
-using FTPConsole.Class.Common;
 using FTPConsole.Class.Dto;
 using FTPLib.Class;
 using FTPLib.Class.Common;
