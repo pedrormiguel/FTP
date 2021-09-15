382df442489cd9d98ec5a375e8e6060364f403c0
Pedro Ruiz <pedrormiguel@outlook.es>
2021-09-15 19:43:58 -0400

    Added new command for add credential server.

--- src/UI/FTPCommandApp/Command/Credential/CredentialsCommand.cs
+++ src/UI/FTPCommandApp/Command/Credential/CredentialsCommand.cs
@@ -1,16 +1,49 @@
-using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
+using System.Threading.Tasks;
+
+using FTPPersistence.Repository;
+using Credentials = CORE.Domain.Entities.Credential;
+using Microsoft.Extensions.Options;
 
 namespace CommandFtpApp.Command.Credential
 {
-    [Command("Credentials")]
+    [Command("Credentials Add", Description = "Add new ftp server with credential")]
     public class CredentialsCommand : ICommand
     {
+        private DbFileHandler _dbFile;
+
+        public CredentialsCommand(IOptions<DbFileHandler> dbFileHandler)
+        {
+            _dbFile = dbFileHandler.Value;
+        }
+
+        [CommandOption("Server", shortName: 's', IsRequired = true, Description = "Url of the FTP Server.")]
+        public string FtpServer { get; init; }
+
+        [CommandOption("User", shortName: 'u', IsRequired = true, Description = "Name of user credential.")]
+        public string UserName { get; init; }
+
+        [CommandOption("Password", shortName: 'p', IsRequired = true, Description = "Password of user credential.")]
+        public string Password { get; init; }
+
+        [CommandOption("Port", Description = "Port of the server.")]
+        public string Port { get; init; } = "21";
+
         public ValueTask ExecuteAsync(IConsole console)
         {
-            console.Output.WriteLine("Working");
+            console.Output.WriteLine("Adding credentials");
+
+            var port = int.Parse(Port);
+            var credential = new Credentials { HostName = this.FtpServer, UserName = this.UserName, Password = this.Password, Port = port };
+            var response = _dbFile.Add(credential);
+
+            console.Output.WriteLine($"Status of the request : {response.Result.Success}");
+
+            if (!response.Result.Success)
+                console.Output.WriteLine($"Error Messages : {response.Result.Message}");
+
             return default;
         }
     }
