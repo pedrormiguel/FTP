4ebebf58fa266d891632afc001c5f5d1b7fae725
Pedro Ruiz <pedrormiguel@outlook.es>
2021-09-19 23:37:37 -0400

    Added test for CommandAdd in Successfully sceneario.

--- src/Test/FTPCommandAppTest/FTPCommandCredentialAdd.cs
+++ src/Test/FTPCommandAppTest/FTPCommandCredentialAdd.cs
@@ -1,25 +1,46 @@
-﻿
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using CliFx;
+using CliFx.Infrastructure;
+using CommandFtpApp.Command.Credential;
+using FTPPersistence.Interfaces;
+using FTPPersistence.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
 using Xunit;
 
 namespace FTPCommandAppTest
 {
-    public class FTPCommandCredentialAdd
+    public class FtpCommandCredentialAdd
     {
-        public FTPCommandCredentialAdd()
+        private readonly CliApplicationBuilder _app;
+
+        public FtpCommandCredentialAdd()
         {
+            var servicesCollection = new ServiceCollection();
+            servicesCollection.AddTransient<IDbFile, DbFileHandler>();
+            servicesCollection.AddTransient<CredentialsCommandAdd>();
+            var provider = servicesCollection.BuildServiceProvider();
+            
+            _app = new CliApplicationBuilder()
+                .AddCommand<CredentialsCommandAdd>()
+                .UseTypeActivator(provider.GetService);
         }
 
         [Fact]
         public async Task Should_Add_Credential_Successfully()
         {
-            //Arrange
+            // Arrange
+            using var console = new FakeInMemoryConsole();
+            var command = new[] {"Credentials Add","--Server","Test2","--User","TEU2","--Password","*****"};
+            var envVars = new Dictionary<string, string>();
 
-            //var commandApp = new FTPCommandCredentialAdd
+            // Act
+            await _app.UseConsole(console).Build().RunAsync(command,envVars);
+            var stdOut = console.ReadOutputString();
 
-            //Act
-
-            //Assert
+            // Assert
+            stdOut.ShouldBe("Adding credentials\nStatus of the request : True\n");
         }
     }
 }
