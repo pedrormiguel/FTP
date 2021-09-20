44f58ba1da6c110687093846432fd06c3209cdb8
Pedro Ruiz <pedrormiguel@outlook.es>
2021-09-19 20:23:00 -0400

    Added class

---
+++ src/Test/FTPCommandAppTest/FTPCommandCredentialAdd.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FTPCommandAppTest
+{
+    public class FTPCommandCredentialAdd
+    {
+        public FTPCommandCredentialAdd()
+        {
+        }
+
+        [Fact]
+        public async Task Should_Add_Credential_Successfully()
+        {
+            //Arrange
+
+            //var commandApp = new FTPCommandCredentialAdd
+
+            //Act
+
+            //Assert
+        }
+    }
+}
