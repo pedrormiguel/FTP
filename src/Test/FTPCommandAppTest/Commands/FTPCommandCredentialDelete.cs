babb661d790dc4293f38ad814d766f12348ff905
Pedro Ruiz <pedrormiguel@outlook.es>
2021-09-27 02:04:01 -0400

    Downgrade the version of .Net.

--- src/Test/FTPCommandAppTest/Commands/FTPCommandCredentialDelete.cs
+++ src/Test/FTPCommandAppTest/Commands/FTPCommandCredentialDelete.cs
@@ -23,7 +23,7 @@ namespace FTPCommandAppTest.Commands
             servicesCollection.AddTransient<IDbFile, DbFileHandler>();
             servicesCollection.AddTransient<CredentialsCommandDelete>();
             _provider = servicesCollection.BuildServiceProvider();
-            
+
             _app = new CliApplicationBuilder()
                 .AddCommand<CredentialsCommandDelete>()
                 .UseTypeActivator(_provider.GetService);
@@ -34,16 +34,16 @@ namespace FTPCommandAppTest.Commands
         {
             // Arrange
             var console = new FakeInMemoryConsole();
-            var credential = new Credential { HostName = "HT", UserName = "UT", Password = "PT", Port = 24};
+            var credential = new Credential { HostName = "HT", UserName = "UT", Password = "PT", Port = 24 };
             var db = _provider.GetRequiredService<IDbFile>();
-            
+
             // Act
             await db.Add(credential);
             var args = new[] { "Credentials Delete", "-I", $"{credential.Id}" };
 
             await _app.UseConsole(console).Build().RunAsync(args);
             var stdOutput = console.ReadOutputString();
-            
+
             // Assert
             stdOutput.ShouldBe($"Element deleted. {credential.HostName}\n");
         }
