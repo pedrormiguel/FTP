b6e204583267cf5b61ebdcce3383afd3aba6c2bb
Pedro Ruiz <pedrormiguel@outlook.es>
2021-09-27 02:18:11 -0400

    We started using the methods to return the route of the file.

--- src/Test/FTPDbFileHandlerTest/FTPDbFileHandler.cs
+++ src/Test/FTPDbFileHandlerTest/FTPDbFileHandler.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CORE.Domain.Entities;
 using FTPLib.Class.Dto;
+using FTPPersistence.Interfaces;
 using FTPPersistence.Repository;
 using Shouldly;
 using Xunit;
@@ -11,15 +12,15 @@ namespace FTPDbFileHandlerTest
 {
     public class FtpDbFileHandler
     {
-        private readonly DbFileHandler _dbFileHandler;
+        private readonly IDbFile _dbFileHandler;
         private readonly string _pathOFile;
         private readonly string _pathOfDirectory;
 
         public FtpDbFileHandler()
         {
             _dbFileHandler = new DbFileHandler();
-            _pathOFile = _dbFileHandler.PathDbFile;
-            _pathOfDirectory = _dbFileHandler.FullRouteOfDirectory;
+            _pathOFile = _dbFileHandler.GetPathFile();
+            _pathOfDirectory = _dbFileHandler.GetPathDirectory();
         }
 
         [Fact]
@@ -48,7 +49,7 @@ namespace FTPDbFileHandlerTest
             response.Error.ShouldBeNullOrEmpty();
             response.Success.ShouldBeTrue();
             response.ValidationErrors.Count.ShouldBe(0);
-            
+
             ValidateAndCleanFile().ShouldBeTrue();
         }
 
@@ -83,7 +84,7 @@ namespace FTPDbFileHandlerTest
             // Arrange
             var credentialTest = new Credential() { HostName = "HNT", Password = "PT", Port = 23, UserName = "UNT" };
             var credentialTest2 = new Credential() { HostName = "HNT2", Password = "PT2", Port = 24, UserName = "UNT2" };
-            
+
             // Act
             await _dbFileHandler.Add(credentialTest);
             await _dbFileHandler.Add(credentialTest2);
@@ -96,17 +97,17 @@ namespace FTPDbFileHandlerTest
             response.Error.ShouldBeNullOrEmpty();
             response.Data.Count().ShouldBe(2);
             response.ValidationErrors.Count.ShouldBe(0);
-            
+
             credentialTestRead.HostName.ShouldBe(credentialTest.HostName);
             credentialTestRead.UserName.ShouldBe(credentialTest.UserName);
             credentialTestRead.Password.ShouldBe(credentialTest.Password);
             credentialTestRead.Port.ShouldBe(credentialTest.Port);
-            
+
             credentialTest2Read.HostName.ShouldBe(credentialTest2.HostName);
             credentialTest2Read.UserName.ShouldBe(credentialTest2.UserName);
             credentialTest2Read.Password.ShouldBe(credentialTest2.Password);
             credentialTest2Read.Port.ShouldBe(credentialTest2.Port);
-            
+
             ValidateAndCleanFile().ShouldBeTrue();
         }
 
@@ -116,20 +117,20 @@ namespace FTPDbFileHandlerTest
             // Arrange
             var credentialTest = new Credential() { HostName = "HNT", Password = "PT", Port = 23, UserName = "UNT" };
             var credentialTest2 = new Credential() { HostName = "HNT2", Password = "PT2", Port = 24, UserName = "UNT2" };
-            
+
             // Act
             await _dbFileHandler.Add(credentialTest);
             await _dbFileHandler.Add(credentialTest2);
             var response = await _dbFileHandler.Delete(credentialTest);
             var counter = await _dbFileHandler.ReadAll();
-            
+
             // Assert
             response.Success.ShouldBeTrue();
             response.Error.ShouldBeNullOrEmpty();
             response.ValidationErrors.Count.ShouldBe(0);
             counter.Data.Count().ShouldBe(1);
             counter.Data.ElementAt(0).ShouldBe(credentialTest2.ToString());
-            
+
             ValidateAndCleanFile().ShouldBeTrue();
         }
 
@@ -139,7 +140,7 @@ namespace FTPDbFileHandlerTest
             // Arrange
             var credentialTest = new Credential() { HostName = "HNT", Password = "PT", Port = 23, UserName = "UNT" };
             const string change = "CHANGED";
-            
+
             // Act
             await _dbFileHandler.Add(credentialTest);
             credentialTest.HostName = change;
@@ -147,17 +148,17 @@ namespace FTPDbFileHandlerTest
             credentialTest.Password = change;
             credentialTest.Port = 21;
             var response = await _dbFileHandler.Update(credentialTest);
-            
+
             // Assert
             response.Success.ShouldBeTrue();
             response.Error.ShouldBeNullOrEmpty();
             response.ValidationErrors.Count.ShouldBe(0);
             response.Data.ShouldBe(credentialTest.ToString());
-            
-            
+
+
             ValidateAndCleanFile().ShouldBeTrue();
         }
-        
+
         private bool ValidateAndCleanFile()
         {
             if (!File.Exists(_pathOFile))
