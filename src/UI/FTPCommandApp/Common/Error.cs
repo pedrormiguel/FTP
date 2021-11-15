45a21b3095ea150205886b8a23c1bbd21129b64e
Pedro <pedro@Pedros-MacBook-Pro.local>
2021-11-15 03:14:15 -0400

    New class to extent Error's Method

--- src/UI/FTPCommandApp/Common/Error.cs
+++ src/UI/FTPCommandApp/Common/Error.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+using CliFx.Infrastructure;
+
 namespace CommandFtpApp.Common
 {
-    public class Error
+    public static class Error
     {
-        
+        public static void ShowError(this IConsole console, string errorMessage, IEnumerable<string> errors)
+        {
+            console.Error.WriteLine($"Not was successful. Error  {errorMessage}");
+
+            foreach (var error in errors)
+            {
+                console.Error.WriteLine($"*{error}\n");
+            }
+        }
     }
 }
